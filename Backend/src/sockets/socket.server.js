9f8af0407cd19b25a53b4df2e2db2a039b177de6
AbhiNav8084 <shubhamkumarsingh415@gmail.com>
2026-05-29 18:37:18 +0530

    upadating chat history

--- Backend/src/sockets/socket.server.js
+++ Backend/src/sockets/socket.server.js
@@ -55,9 +55,9 @@ function initSocketServer(httpServer) {
                 role: "user"
             })
 
-            const chatHistory = await messageModel.find({
+            const chatHistory = (await messageModel.find({
                 chat: messagePayload.chat
-            })
+            }).sort({ createdAt: -1 }).limit(20).lean()).reverse()
 
             const response = await aiService.generateResponse(chatHistory.map(item =>{
                 return{
@@ -80,8 +80,6 @@ function initSocketServer(httpServer) {
         })
 
     })
-
-
 
 }
 
